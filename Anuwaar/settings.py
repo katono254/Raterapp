54ab4382156525f8d8d5387341fb9253aecc81d9
Greg <blackrocket.47@gmail.com>
2020-02-19 14:43:51 +0300

    Added upload care in the app

--- Anuwaar/settings.py
+++ Anuwaar/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'pyuploadcare.dj',
 
 ]
+UPLOADCARE = {
+    'pub_key': '2b709bca64245dd9e55e',
+    'secret': '0a60851de5f3db2dc728',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
