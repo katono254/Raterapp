313048a0f5807e8e8d652b32a0b99920f3769882
Greg <blackrocket.47@gmail.com>
2020-02-20 10:33:21 +0300

    Modified the database

--- Anuwaar/settings.py
+++ Anuwaar/settings.py
@@ -30,6 +30,34 @@ SECRET_KEY = 'd=%=&u@40$=_c1t*+_efr_y3a7l!l65$a&s)pvsaeqpgz9^x&!'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -89,14 +117,6 @@ WSGI_APPLICATION = 'Anuwaar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rater',
-        'USER': 'greg',
-    'PASSWORD':'greg1234',
-    }
-}
 
 
 # Password validation
