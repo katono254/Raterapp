817a1a919eaca896b5fc24ca5edd5ef724af7243
Greg <blackrocket.47@gmail.com>
2020-02-20 11:43:26 +0300

    Comment out the page

--- Rater/views.py
+++ Rater/views.py
@@ -22,7 +22,7 @@ def index(request):
 
     try:
         posts = Post.objects.all()
-        posts = posts[::-1]
+        #posts = posts[::-1]
         a_post = random.randint(0, len(posts)-1)
         random_post = posts[a_post]
         print(random_post.photo)
